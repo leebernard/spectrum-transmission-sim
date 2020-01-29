8b20cac4a03dd74d54134531e1ab2d50c37abaa4
leebernard <ldbernar@ucsc.edu>
2020-01-28 16:41:38 -0800

    Generate the spectrum wavelengths, and plot a section

--- spectrum_generation.py
+++ spectrum_generation.py
@@ -7,4 +7,22 @@ idealized output from a spectrograph
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+from astropy.io import fits
+
+# with fits.open('sun.fits') as hdul:
+hdul = fits.open('sun.fits')
+sun = hdul[0]
+
+pix_per_angstrom = sun.header['CDELT1']
+intial_angstrom = sun.header['CRVAL1']
+
+# generate an x-data set
+angstrom = np.arange(0, sun.data.size, step=1) * pix_per_angstrom + intial_angstrom
+
+plt.figure('Solar Spetrum in Angstroms')
+plot_start = int(sun.data.size*.4)
+plot_end = int(sun.data.size*.41)
+plt.scatter(angstrom[plot_start:plot_end], sun.data[plot_start:plot_end], s=1)
 
