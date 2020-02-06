ce033555eee81fa3e2aba5f2f071296bb55e8b77
leebernard <ldbernar@ucsc.edu>
2020-02-06 15:16:43 -0800

    Now using proper ndimage routines

--- spectrum_generation.py
+++ spectrum_generation.py
@@ -11,7 +11,6 @@ import matplotlib.pyplot as plt
 
 from astropy.io import fits
 import astropy.convolution as conv
-from scipy import signal
 from scipy.ndimage import gaussian_filter
 from scipy.ndimage.filters import _gaussian_kernel1d
 # from scipy import integrate
@@ -26,8 +25,9 @@ intial_angstrom = sun.header['CRVAL1']
 # generate an x-data set
 angstrom = np.arange(0, sun.data.size, step=1) * angstrom_per_pix + intial_angstrom
 
+
 plot_start = int(sun.data.size*0.0)
-plot_end = int(sun.data.size*.15)
+plot_end = int(sun.data.size*1.0)
 # take a slice of data
 sun_slice = sun.data[plot_start:plot_end]
 angstrom_slice = angstrom[plot_start:plot_end]
@@ -38,32 +38,26 @@ angstrom_slice = angstrom[plot_start:plot_end]
 # # normalize the kernal
 # sinc_kernel = sinc_kernel/np.sum(sinc_kernel)
 
-# generate a gaussian kernel
-resolution = 50  # the resolution of the spectrum in angstroms. This corresponds to FWHM
-sigma = resolution/2.35482
-gauss_kernel = signal.gaussian(sun_slice.size, sigma)
-# normalize
-gauss_kernel = gauss_kernel * 1/np.sum(gauss_kernel)
-
-plt.figure('kernel plot')
-plt.scatter(angstrom_slice, gauss_kernel, s=2)
-
-# convovle the signal with a low pass filter
-lp_sun = signal.convolve(sun_slice, gauss_kernel, mode='same')
-# '''
 
 # filter the data
-resolution = 50 # /.002  # the resolution of the spectrum in angstroms. This corresponds to FWHM
+resolution = 500 # /.002  # the resolution of the spectrum in angstroms. This corresponds to FWHM
 sigma = resolution/2.35482
 lp_sun = gaussian_filter(sun_slice, sigma)
+
+'''
+# this is just test data, to check the kernel the filter is using
 lw = int(4.0 * float(sigma) + 0.5)
 gauss_kernel = _gaussian_kernel1d(sigma, order=0, radius=lw)
+'''
 
 plt.figure('Solar Spetrum in Angstroms')
 plt.scatter(angstrom_slice, sun_slice, s=1)
 # plt.figure('filtered spectrum')
 plt.scatter(angstrom_slice, lp_sun, s=2)
-plt.xlim(3950, 3960)
+plt.xlim(3900, 3960)
 
-
-
+# filter the full spectrum
+filtered_sun = gaussian_filter(sun.data, sigma)
+plt.figure('Full available filtered solar spectrum')
+plt.scatter(angstrom, sun.data, s=1)
+plt.scatter(angstrom, filtered_sun, s=1)
