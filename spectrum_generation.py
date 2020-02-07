af8f9bfeb12f95af27f08771b0c377dc542127d9
leebernard <ldbernar@ucsc.edu>
2020-02-06 16:52:58 -0800

    found a testable feature at 5456 angs

--- spectrum_generation.py
+++ spectrum_generation.py
@@ -25,12 +25,6 @@ intial_angstrom = sun.header['CRVAL1']
 # generate an x-data set
 angstrom = np.arange(0, sun.data.size, step=1) * angstrom_per_pix + intial_angstrom
 
-
-plot_start = int(sun.data.size*0.0)
-plot_end = int(sun.data.size*1.0)
-# take a slice of data
-sun_slice = sun.data[plot_start:plot_end]
-angstrom_slice = angstrom[plot_start:plot_end]
 # '''
 # filter_factor = 10
 # x = np.linspace(-5*np.pi, 5*np.pi, num=sun_slice.size)  # generate values over 10 periods
@@ -38,26 +32,38 @@ angstrom_slice = angstrom[plot_start:plot_end]
 # # normalize the kernal
 # sinc_kernel = sinc_kernel/np.sum(sinc_kernel)
 
-
-# filter the data
-resolution = 500 # /.002  # the resolution of the spectrum in angstroms. This corresponds to FWHM
-sigma = resolution/2.35482
-lp_sun = gaussian_filter(sun_slice, sigma)
-
 '''
 # this is just test data, to check the kernel the filter is using
 lw = int(4.0 * float(sigma) + 0.5)
 gauss_kernel = _gaussian_kernel1d(sigma, order=0, radius=lw)
 '''
 
-plt.figure('Solar Spetrum in Angstroms')
-plt.scatter(angstrom_slice, sun_slice, s=1)
-# plt.figure('filtered spectrum')
-plt.scatter(angstrom_slice, lp_sun, s=2)
-plt.xlim(3900, 3960)
-
 # filter the full spectrum
+resolution = 500  # /.002  # the resolution of the spectrum in angstroms. This corresponds to FWHM
+sigma = resolution/2.35482
 filtered_sun = gaussian_filter(sun.data, sigma)
-plt.figure('Full available filtered solar spectrum')
+
+plt.figure('Full available solar spectrum')
 plt.scatter(angstrom, sun.data, s=1)
 plt.scatter(angstrom, filtered_sun, s=1)
+
+# find a slice of data
+start_ang = 5000
+end_ang = angstrom[-1]
+
+plot_start = np.where(angstrom == start_ang)[0][0]
+plot_end = np.where(angstrom == end_ang)[0][0]
+sun_slice = filtered_sun[plot_start:plot_end]
+angstrom_slice = angstrom[plot_start:plot_end]
+
+plt.figure('Slice of Filtered Solar Spectrum')
+plt.scatter(angstrom_slice, sun_slice, s=1)
+plt.xlim(5000, 5500)
+plt.xlim(5500, 6000)
+plt.xlim(6000, 6500)
+plt.xlim(6500, 7000)
+plt.xlim(7000, angstrom[-1])
+
+plt.xlim(5400, 5500)  # fairly isolated feature at 5456 angs
+plt.xlim(5454, 5457)
+
