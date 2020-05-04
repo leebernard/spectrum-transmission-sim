f45e3cd8a387db2294746b07cf8bfc6d5894197d
leebernard <ldbernar@ucsc.edu>
2020-05-04 10:53:28 -0700

    More plot generation stuff

--- generate_plots.py
+++ generate_plots.py
@@ -5,16 +5,28 @@ from astropy.io import fits
 from spectrum_fitter import spectrum_gaussian_fit
 
 # open the files
+# bffalse_name = 'spectrum_sim_gaussdouble_bffalse.fits'
+# bftrue_name = 'spectrum_sim_gaussdouble_bftrue.fits'
+#
+# bffalse_name = 'spectrum_sim_gaussian_bffalse3.fits'
+# bftrue_name = 'spectrum_sim_gaussian_bftrue3.fits'
+#
+# bffalse_name = 'spectrum_sim_gausshalf_bffalse.fits'
+# bftrue_name = 'spectrum_sim_gausshalf_bftrue.fits'
 
-with fits.open('spectrum_sim_gaussian_bffalse3.fits') as hdul:
+bffalse_name = 'spectrum_sim_gauss_transpose_bffalse.fits'
+bftrue_name = 'spectrum_sim_gauss_transpose_bftrue.fits'
+
+with fits.open(bffalse_name) as hdul:
     galsim_sensor_image = hdul[0].data.copy()
 
-with fits.open('spectrum_sim_gaussian_bftrue3.fits') as hdul:
+with fits.open(bftrue_name) as hdul:
     galsim_bf_image = hdul[0].data.copy()
 
 difference_image = galsim_sensor_image[:, 5:-5] - galsim_bf_image[:, 5:-5]
 
 plt.figure('GalSim simulated spectrum image')
+plt.suptitle(bftrue_name)
 
 plt.subplot(311)
 plt.imshow(galsim_sensor_image[:, 5:-5], cmap='viridis')
@@ -52,6 +64,7 @@ print('Difference in mean:', mean_difference)
 
 
 plt.figure('mean profile analysis')
+plt.suptitle(bftrue_name)
 
 plt.subplot(311)
 plt.plot(mean_nobf_pixels, mean_sensor_profile)
@@ -74,10 +87,47 @@ plt.xlabel('(pixels)')
 plt.ylabel('(flux)')
 plt.legend(('Data', 'Fits'))
 
+
+# analyze the fit residuals
+# sum in quadtrature
+total1 = np.sqrt(sum(np.square(g_nobf(mean_nobf_pixels) - mean_sensor_profile)))
+total2 = np.sqrt(sum(np.square(g_withbf(mean_bf_pixels) - mean_bf_profile)))
+
+
+# normalize the data sets
+norm_g_nobf = g_nobf(mean_nobf_pixels) / sum(g_nobf(mean_nobf_pixels))
+norm_data_nobf = mean_sensor_profile / sum(mean_sensor_profile)
+no_bf_residuals = np.sqrt(sum(np.square(norm_g_nobf - norm_data_nobf)))
+
+norm_g_yesbf = g_withbf(mean_bf_pixels) / sum(g_withbf(mean_bf_pixels))
+norm_data_yesbf = mean_bf_profile / sum(mean_bf_profile)
+yes_bf_residuals = np.sqrt(sum(np.square(norm_g_yesbf - norm_data_yesbf)))
+
+print('Sanity check:')
+print('ratio of residuals between no bf and with bf')
+print('raw summing', total2/total1)
+print('summing after normalizing', yes_bf_residuals/no_bf_residuals)
+
+print('Normalized residual values')
+print('no BF:', no_bf_residuals)
+print('yes BF:', yes_bf_residuals)
+
 plt.figure('Fit Residuals')
+plt.suptitle(bftrue_name)
+
+plt.subplot(311)
+plt.plot(g_nobf(mean_nobf_pixels) - mean_sensor_profile)
+plt.title('Residuals of the noBF fit')
+plt.ylabel(r'(flux? ergs?)')
+
+plt.subplot(312)
+plt.plot(g_withbf(mean_bf_pixels) - mean_bf_profile)
+plt.title('Residuals of the yesBF fit')
+plt.ylabel(r'(flux? ergs?)')
+
+plt.subplot(313)
 plt.plot(g_withbf(mean_bf_pixels) - g_nobf(mean_nobf_pixels))
-#plt.subplot()
-plt.title('residuals of fits, after fixing .002px goof')
+plt.title('BF fit minus noBF fit')
 plt.xlabel('(pixels)')
 plt.ylabel('(flux)')
 
