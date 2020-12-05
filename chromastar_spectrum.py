25d3dae464a9df5a7cc1174ee361c5d3ca00615b
leebernard <ldbernar@ucsc.edu>
2020-12-05 14:01:58 -0700

    fixed spectrum_slicer to be more robust

--- chromastar_spectrum.py
+++ chromastar_spectrum.py
@@ -10,12 +10,14 @@ import matplotlib.pyplot as plt
 # from astropy.io import fits
 # from astropy.modeling import models, fitting
 from scipy.ndimage import gaussian_filter
+from scipy import constants
+from scipy.interpolate import griddata
 # from scipy.ndimage import gaussian_filter1d
 
 from toolkit import spectrum_slicer
 
 
-filename = '../ast_521/ChromaStarPy/Outputs/Project-5777.0-4.44-0.0-588.0-592.0-Run.sed.txt'
+filename = '../ast_521/ChromaStarPy/Outputs/BF_sim-5777.0-4.44-0.0-588.0-750.0-Run.sed.txt'
 
 with open(filename, 'r') as file:
     raw_header = file.readline()
@@ -40,15 +42,47 @@ with open(filename, 'r') as file:
 
 spectrum_flux = 10**log_flux
 
+# convert from ergs/μm to watts/nm
+spectrum_flux = spectrum_flux * 10**-7 * 10**-3
+
+# convert from flux/m^2 to photons/m^2
+c = constants.speed_of_light
+h = constants.h
+
+spectrum_counts = spectrum_flux * nanometers / (h*c)
+
+'''
 # filter the full spectrum
 resolution = 50  # the resolution of the spectrum in nanometers. This corresponds to FWHM
 sigma = resolution/(2.0 * np.sqrt(2.0 * np.log(2.0)))
 filtered_flux = gaussian_filter(spectrum_flux.data, sigma)
+filtered_counts = gaussian_filter(spectrum_counts.data, sigma)
 
 plt.figure('Full available solar spectrum')
-plt.scatter(nanometers, spectrum_flux, s=1)
-plt.scatter(nanometers, filtered_flux, s=1)
+plt.scatter(nanometers, spectrum_counts, s=1)
+plt.scatter(nanometers, filtered_counts, s=1)
 plt.title('Slice of Solar spectrum')
 plt.xlabel('Angstroms')
 plt.legend(['Before smoothing', 'After smoothing'])
 # plt.xlim(585, 595)
+'''
+
+# grab a slice of data
+spectrum_start = 588
+spectrum_end = 750
+nm_slice, spectrum_slice = spectrum_slicer(spectrum_start,
+                                           spectrum_end,
+                                           nanometers,
+                                           spectrum_counts)
+
+# interpolate the data to even spacing
+sim_nm_per_pixel = .35
+
+number_pixels = int((nm_slice[-1] - nm_slice[0])/sim_nm_per_pixel)
+pixel_nanometers = np.linspace()
+
+xi = np.linspace(nanometers[0], nanometers[-1], 500)
+test = griddata(nanometers, spectrum_counts, xi=xi)
+
+
+
--- toolkit.py
+++ toolkit.py
@@ -6,12 +6,14 @@ import numpy as np
 
 
 def spectrum_slicer(start_angstrom, end_angstrom, angstrom_data, spectrum_data):
-    start_index = np.where(angstrom_data == start_angstrom)[0][0]
-    end_index = np.where(angstrom_data == end_angstrom)[0][0]
+    start_index = (np.abs(angstrom_data - start_angstrom)).argmin()
+    end_index = (np.abs(angstrom_data - end_angstrom)).argmin()
     spectrum_slice = spectrum_data[start_index:end_index]
     angstrom_slice = angstrom_data[start_index:end_index]
 
     return angstrom_slice, spectrum_slice
+
+
 
 
 
