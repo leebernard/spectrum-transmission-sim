8647094fec48db04e178c63db7eb13dd50d1bdb7
leebernard <ldbernard@asu.edu>
2021-03-12 12:29:26 -0700

    Added some documentation

--- planet_sim/transit_sim.py
+++ planet_sim/transit_sim.py
@@ -5,13 +5,38 @@ from matplotlib.ticker import FormatStrFormatter
 
 
 def z_lambda(sigma, scale_h, p0, planet_radius, mass, T, g):
+    '''
+
+    Parameters
+    ----------
+    sigma: array
+        Absorption cross section of atmosphere species as a function of wavelength
+    scale_h: float
+        Scale height of atmosphere
+    p0: float
+        Reference pressure of atmosphere; pressure at z=0
+    planet_radius: float
+        Minimum radius of planet
+    mass:
+        mass of planet
+    T:
+        Effective temperature of planet
+    g:
+        Graviational constant of planet. Assumes thin shell of atmosphere
+
+    Returns
+    -------
+    z: float, array
+        The amount by which the planet's occultation disk is increased by
+        opacity of the atmosphere, as a function of wavelength.
+    '''
     # constants
     k = 1.38e-23  # boltzmann constant k_b in J/K
     amu_kg = 1.66e-27  # kg/amu
 
     # set mixing ratio to 1
     xi = 1
-    # set equiv scale hight to 1
+    # set equiv scale height to 1
     tau_eq = 1
 
     # calculate beta
@@ -19,7 +44,10 @@ def z_lambda(sigma, scale_h, p0, planet_radius, mass, T, g):
     return scale_h * np.log(xi * sigma * 1/np.sqrt(k*mass*amu_kg*T*g) * beta)
 
 
-def alpha_lambda(star_radius, planet_radius, z):
+def alpha_lambda(sigma, planet_radius, scale_h, p0, T, mass, g, star_radius):
+
+    z = z_lambda(sigma, scale_h, p0, planet_radius, mass, T, g)
+
     return (planet_radius / star_radius)**2 + (2 * planet_radius * z)/(star_radius**2)
 
 
@@ -43,24 +71,26 @@ with open(filename) as file:
 cross_wavelengths = 1e7/wave_numbers
 
 # star and planet radii
-r_earth = 6.3710e6
-r_sun = 6.957e8
-g_earth = 10
+r_earth = 6.3710e6  # meters
+r_sun = 6.957e8  # meters
+g_earth = 10  # m/s
 
 # using data from Gliese 876 d, pulled from Wikipedia
 rad_planet = 1.65  # earth radii
 rad_star = .376  # solar radii
 m_planet = 6.8  # in earth masses
 
-# fuck it, use made up shit
 # data from Kepler-10c
 rad_planet = 2.35
 m_planet = 7.37
 
-
+# fuck it, use made up shit
+# assuming relationship of r = m^0.55
+rad_planet = 3.5
+m_planet = 10
 
 # reference pressure: 1 barr
-p0 = 1 * 1e5
+p_earth = 1 * 1e5
 
 # scale height
 k = 1.38e-23  # boltzmann constant k_b in J/K
@@ -70,26 +100,25 @@ T = 290  # K
 mass = 18  # amu
 H = k*T/(mass*amu_kg * g)
 
-# scale reference pressure up
-# maybe later
 
 r_p = r_earth * rad_planet
 r_star = r_sun * rad_star
 baseline_depth = (r_p/r_star)**2
+# scale reference pressure up
+# maybe later
+p0 = p_earth * 10
 
-transit_depth = alpha_lambda(star_radius=r_star,
+transit_depth = alpha_lambda(sigma=cross_sections,
                              planet_radius=r_p,
-                             z=z_lambda(sigma=cross_sections,
-                                        scale_h=H,
-                                        p0=p0,
-                                        planet_radius=r_p,
-                                        mass=mass,
-                                        T=T,
-                                        g=g)
-                             )
+                             scale_h=H,
+                             p0=p0,
+                             T=T,
+                             mass=mass,
+                             g=g,
+                             star_radius=r_star,)
 
 # generate photon noise from a signal value
-signal = 1.22e8
+signal = 1.22e6
 photon_noise = 1/np.sqrt(signal)  # calculate noise as fraction of signal
 noise = np.random.normal(scale=photon_noise, size=transit_depth.size)
 
