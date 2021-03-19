3c01d219b6cc2b97d6eb8fd41c709e7c222da900
leebernard <ldbernard@asu.edu>
2021-03-19 16:12:18 -0700

    Added a function for opening cross-section data

--- planet_sim/transit_sim.py
+++ planet_sim/transit_sim.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
 from planet_sim.transit_toolbox import alpha_lambda
+from planet_sim.transit_toolbox import open_cross_section
 from planet_sim.transit_toolbox import scale_h
 
 '''
@@ -17,18 +18,12 @@ Everything is 1D...
 Future expansions needed:
 Account for temperature structure in scale height
 '''
-# filename = './line_lists/1H2-16O_6250-12500_300K_20.000000.sigma'
-filename = './line_lists/1H2-16O_6250-12500_300K_100.000000.sigma'
-with open(filename) as file:
-    raw_data = file.readlines()
-    wave_numbers = []
-    cross_sections = []
-    for x in raw_data:
-        wave_string, cross_string = x.split()
-        wave_numbers.append(float(wave_string))
-        cross_sections.append(float(cross_string))
-    wave_numbers = np.array(wave_numbers)
-    cross_sections = np.array(cross_sections)
+
+water_data_file = './line_lists/1H2-16O_4000-10000_1500K_100.000000.sigma'
+wave_numbers, cross_sections = open_cross_section(water_data_file)
+
+h2_data_file = './line_lists/1H2_4000-10000_1500K_100.000000.sigma'
+_, h2_cross_sections = open_cross_section(h2_data_file)
 
 # convert wavenumber to wavelength
 cross_wavelengths = 1e7/wave_numbers
@@ -53,19 +48,35 @@ T = 290  # K
 mass = 18  # amu
 
 
+# hot jupiter time!
+p0 = 10
+T = 1500
+water_ratio = .01
+
+# need to calculate average molecular mass of atmosphere
+mass_water = 18
+h2he_ratio = .17
+mass_h2he = (1 - h2he_ratio)*2 + h2he_ratio*4
+mass = (1 - water_ratio)*mass_h2he + water_ratio*mass_water
+
+# temporary mass cause I don't have He cross sections yet
+placeholder_mass = (1 - water_ratio)*2 + water_ratio*mass_water
+
 # baseline_depth = (r_p/r_star)**2
 # scale reference pressure up
 # maybe later
 
 
 transit_depth = alpha_lambda(sigma_trace=cross_sections,
-                             xi=1,
+                             xi=water_ratio,
                              planet_radius=rad_planet,
                              p0=p0,
                              T=T,
                              mass=mass,
                              planet_mass=m_planet,
-                             star_radius=rad_star)
+                             star_radius=rad_star,
+                             sigma_filler=h2_cross_sections
+                             )
 
 # generate photon noise from a signal value
 signal = 1.22e6
@@ -80,10 +91,10 @@ spec_res = np.mean(np.diff(np.flip(cross_wavelengths)))
 
 plt.figure('transit depth %.2f' %spec_res, figsize=(8, 8))
 plt.subplot(212)
-plt.plot(cross_wavelengths, cross_sections)
+plt.plot(cross_wavelengths, np.log(cross_sections))
 plt.title('Cross section of H2O')
 plt.xlabel('Wavelength (nm)')
-plt.ylabel('cm^2/molecule')
+plt.ylabel('ln[cm^2/molecule]')
 
 plt.subplot(211)
 plt.plot(cross_wavelengths, transit_depth)
