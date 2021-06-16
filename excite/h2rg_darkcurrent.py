a46aca2cc141ef714edfc9a9c6065248b0a86ada
leebernard <ldbernard@asu.edu>
2021-06-16 08:20:58 -0700

    Made a data curve of dark current jitter-induced noise

--- excite/h2rg_darkcurrent.py
+++ excite/h2rg_darkcurrent.py
@@ -76,7 +76,7 @@ def generate_T_noise(time, scale, observation_duration, noise_freq, verbose=Fals
 # pick length of observation run
 observation_duration = 3600  # 1 hour, in seconds
 interpolated_sample_rate = 1000  # in Hz
-time = np.linspace(0, observation_duration, num=observation_duration * interpolated_sample_rate)
+fine_time = np.linspace(0, observation_duration, num=observation_duration * interpolated_sample_rate)
 
 # noise parameters
 noise_freq = 50  # in Hz
@@ -87,17 +87,78 @@ scale = .005
 observation_sample_rate = 1
 sample_time = np.linspace(0, observation_duration, num=observation_duration * observation_sample_rate)
 
-T_40 = 40 + generate_T_noise(time, scale, observation_duration, noise_freq)
-T_50 = 50 + generate_T_noise(time, scale, observation_duration, noise_freq)
-T_60 = 60 + generate_T_noise(time, scale, observation_duration, noise_freq)
+T_40, _ = instrument_non_uniform_tophat(sample_time, fine_time,
+                                        Fp=40 + generate_T_noise(fine_time, scale, observation_duration, noise_freq))
+
+T_50, _ = instrument_non_uniform_tophat(sample_time, fine_time,
+                                        Fp=50 + generate_T_noise(fine_time, scale, observation_duration, noise_freq))
+
+T_60, _ = instrument_non_uniform_tophat(sample_time, fine_time,
+                                        Fp=60 + generate_T_noise(fine_time, scale, observation_duration, noise_freq))
+
+T_70, _ = instrument_non_uniform_tophat(sample_time, fine_time,
+                                        Fp=70 + generate_T_noise(fine_time, scale, observation_duration, noise_freq))
+
 
 
 # plot the temperature data
 time_stop = 300
 plt.figure('Temperature_plot')
-plt.scatter(time[:time_stop], T_40[:time_stop], label='T=40K')
-plt.scatter(time[:time_stop], T_50[:time_stop], label='T=50K')
-plt.scatter(time[:time_stop], T_60[:time_stop], label='T=60K')
+plt.scatter(sample_time[:time_stop], T_40[:time_stop], label='T=40K')
+
+# generate dark current values
+dark_40 = i_dark(T_40)
+dark_50 = i_dark(T_50)
+dark_60 = i_dark(T_60)
+dark_70 = i_dark(T_70)
+
+# plot the dark current noise
+plt.figure('dark current')
+plt.plot(sample_time, dark_40, label='T=40K')
+# plt.plot(sample_time, dark_50, label='T=50K')
+# plt.plot(sample_time, dark_60, label='T=60k')
+plt.xlim(right=time_stop)
 plt.legend()
+
+
+# generate a cube of data, for 40-70 degrees, every 2 degrees
+temp_curve = np.linspace(40, 70, num=15)
+
+observation_duration = 1800  # 1/2 hour, in seconds
+interpolated_sample_rate = 1000  # in Hz
+fine_time = np.linspace(0, observation_duration, num=observation_duration * interpolated_sample_rate)
+
+# noise parameters
+noise_freq = 50  # in Hz
+scale = .005
+
+# generate sampling points
+# sampling of data, in Hz
+observation_sample_rate = 1
+sample_time = np.linspace(0, observation_duration, num=observation_duration * observation_sample_rate)
+
+# generate temperature data cube
+temp_data_cube = []
+for temp_value in temp_curve:
+    temp_data, _ = instrument_non_uniform_tophat(sample_time, fine_time,
+                                                    Fp=temp_value + generate_T_noise(fine_time, scale, observation_duration, noise_freq))
+    temp_data_cube.append(temp_data)
+
+temp_data_cube = np.array(temp_data_cube)
+
+# generate dark current data cube
+dc_cube = i_dark(temp_data_cube)
+
+
+mean_dc = np.mean(dc_cube, axis=1)
+stddev_dc = np.std(dc_cube, axis=1)
+dc_sn = mean_dc/stddev_dc
+plt.figure('dark current temp curve')
+plt.scatter(temp_curve, mean_dc, label='mean dark current')
+
+plt.figure('dark current sn')
+plt.scatter(temp_curve, dc_sn)
+
+
 
 
