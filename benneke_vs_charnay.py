b9d802d68cfdc9c8afb7f3e79abd332b538e35ba
leebernard <ldbernard@asu.edu>
2022-01-21 11:58:59 -0700

    Started another senario for atmosphere transmission

---
+++ benneke_vs_charnay.py
@@ -0,0 +1,14 @@
+"""
+Charnay: https://ui.adsabs.harvard.edu/abs/2021A%26A...646A.171C/abstract
+Bezard (non-gcm): https://ui.adsabs.harvard.edu/abs/2020arXiv201110424B/abstract
+
+Benneke: https://ui.adsabs.harvard.edu/abs/2019ApJ...887L..14B/abstract
+
+Notes:
+    Need to get line lists for 600 Kelvin
+"""
+
+
+
+
+
