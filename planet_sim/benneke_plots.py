8bb5fc018f800b089c05db3e07efce85046513a1
leebernard <ldbernard@asu.edu>
2022-02-26 19:40:46 -0700

    added Nat's spot0.py

--- planet_sim/benneke_plots.py
+++ planet_sim/benneke_plots.py
@@ -2,8 +2,8 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = '/home/lee/PycharmProjects/spectrum-transmission-sim/planet_sim/sim_results/benneke_h2o_vs_ch4_1_compact_retrieval.pkl'
-
+# filename = '/home/lee/PycharmProjects/spectrum-transmission-sim/planet_sim/sim_results/benneke_h2o_vs_ch4_1_compact_retrieval.pkl'
+filename = '/home/lee/PycharmProjects/spectrum-transmission-sim/planet_sim/sim_results/benneke_h2o_vs_ch4_noise125_compact_retrieval.pkl'
 with open(filename, 'rb') as f:
     benneke_archive = pickle.load(f)
 
@@ -39,25 +39,25 @@ hist_ax.legend()
 
 
 '''Make Receiver Operator Characteristic curves'''
-dynamic_range = np.linspace(delta_logz_noN.min(), delta_logz_N.max(), num=50)
+dynamic_range = np.linspace(delta_logz_ch4true.min(), delta_logz_h2otrue.max(), num=50)
 
 false_negs = np.empty(dynamic_range.shape)
 for i, bayes_criteria in enumerate(dynamic_range):
-    false_negs[i] = np.sum(delta_logz_N < bayes_criteria) / delta_logz_N.size
-    # print(i, np.sum(delta_logz_N < bayes_criteria))
+    false_negs[i] = np.sum(delta_logz_h2otrue < bayes_criteria) / delta_logz_h2otrue.size
+    # print(i, np.sum(delta_logz_h2otrue < bayes_criteria))
 
 true_negs = np.empty(dynamic_range.shape)
 for i, bayes_criteria in enumerate(dynamic_range):
-    true_negs[i] = np.sum(delta_logz_noN < bayes_criteria) / delta_logz_noN.size
-    # print(i, np.sum(delta_logz_noN < bayes_criteria))
+    true_negs[i] = np.sum(delta_logz_ch4true < bayes_criteria) / delta_logz_ch4true.size
+    # print(i, np.sum(delta_logz_ch4true < bayes_criteria))
 
 true_pos = np.empty(dynamic_range.shape)
 for i, bayes_criteria in enumerate(dynamic_range):
-    true_pos[i] = np.sum(delta_logz_N > bayes_criteria) / delta_logz_N.size
+    true_pos[i] = np.sum(delta_logz_h2otrue > bayes_criteria) / delta_logz_h2otrue.size
 
 false_pos = np.empty(dynamic_range.shape)
 for i, bayes_criteria in enumerate(dynamic_range):
-    false_pos[i] = np.sum(delta_logz_noN > bayes_criteria) / delta_logz_noN.size
+    false_pos[i] = np.sum(delta_logz_ch4true > bayes_criteria) / delta_logz_ch4true.size
 
 # roc_fig, (pos_ax, negs_ax) = plt.subplots(1, 2, figsize=(12, 6))
 roc_fig, pos_ax = plt.subplots(1, figsize=(6,6))
@@ -71,16 +71,16 @@ pos_ax.set_ylabel('True Positive')
 diag = np.linspace(0, 1)
 pos_ax.plot(diag, diag, color='r')
 
-true_pos_2sigma = np.sum(delta_logz_N > 0.9) / delta_logz_N.size
-false_pos_2sigma = np.sum(delta_logz_noN > 0.9) / delta_logz_noN.size
+true_pos_2sigma = np.sum(delta_logz_h2otrue > 0.9) / delta_logz_h2otrue.size
+false_pos_2sigma = np.sum(delta_logz_ch4true > 0.9) / delta_logz_ch4true.size
 pos_ax.scatter(false_pos_2sigma, true_pos_2sigma, color='r', marker='d', label='Δlog(z) = 0.9 (2σ)')
 
-true_pos_36sigma = np.sum(delta_logz_N > 5) / delta_logz_N.size
-false_pos_36sigma = np.sum(delta_logz_noN > 5) / delta_logz_noN.size
+true_pos_36sigma = np.sum(delta_logz_h2otrue > 5) / delta_logz_h2otrue.size
+false_pos_36sigma = np.sum(delta_logz_ch4true > 5) / delta_logz_ch4true.size
 pos_ax.scatter(false_pos_36sigma, true_pos_36sigma, color='purple', marker='d', label='Δlog(z) = 5 (3.6σ)')
 
-true_pos_36sigma = np.sum(delta_logz_N > 11) / delta_logz_N.size
-false_pos_36sigma = np.sum(delta_logz_noN > 11) / delta_logz_noN.size
+true_pos_36sigma = np.sum(delta_logz_h2otrue > 11) / delta_logz_h2otrue.size
+false_pos_36sigma = np.sum(delta_logz_ch4true > 11) / delta_logz_ch4true.size
 pos_ax.scatter(false_pos_36sigma, true_pos_36sigma, color='black', marker='d', label='Δlog(z) = 11 (5σ)')
 
 pos_ax.axvline(0.05, linestyle='--', color='r', label='5% false positive rate')
